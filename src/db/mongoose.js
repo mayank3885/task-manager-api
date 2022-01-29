d7da7ca1c2ffac2b80427bbee4c228e4cc5e2f82
mayank makwana <tinythanos8@gmail.com>
2022-01-29 19:06:34 +0530

    Trying to deploy 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,12 @@
-require('dotenv').config;
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(()=>{
+    console.log('Mongodb succesfully connected')
+}).catch(()=>[
+    console.log('An Error occured')
+])
