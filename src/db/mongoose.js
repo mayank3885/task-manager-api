b88973151d7181ee79acb66f1b32703d6addc410
mayank makwana <tinythanos8@gmail.com>
2022-01-29 20:48:05 +0530

    tried once again

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
-}).then(()=>{
-    console.log('Mongodb succesfully connected')
-}).catch(()=>[
-    console.log('An Error occured')
-])
+mongoose.connect(process.env.MONGODB_URL)
